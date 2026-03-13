00c23eb16948efe9d350ffc0ca68d10972a2750c
Leonardo Gonzalez <gonzalezrujano@gmail.com>
2026-03-13 14:20:27 -0400

    feat: show image for categories

--- app/Http/Controllers/Frontend/CategoryController.php
+++ app/Http/Controllers/Frontend/CategoryController.php
@@ -19,32 +19,63 @@ class CategoryController extends Controller
     public function index()
     {
         $selectedTheme = getTheme();
-        $pageSeo = Page::where('template_name', $selectedTheme)->where('slug', 'category')->first();
-        $pageSeo['breadcrumb_image'] = $pageSeo?->breadcrumb_status == 1 ?  getFile($pageSeo->breadcrumb_image_driver, $pageSeo->breadcrumb_image) : null;
-
-        $categories = ListingCategory::select('id','icon')->with(['details:id,listing_category_id,language_id,name'])->where('status',1)->latest()->get();
-        $categorySingle = DB::table('content_details')
-            ->join('contents', 'contents.id', '=', 'content_details.content_id')
-            ->where('contents.theme', $selectedTheme)
-            ->where('contents.name', 'listing_categories')
-            ->where('contents.type', 'single')
+        $pageSeo = Page::where("template_name", $selectedTheme)
+            ->where("slug", "category")
             ->first();
-        return view(template(). 'frontend.category.index', compact('pageSeo', 'categories','categorySingle'));
+        $pageSeo["breadcrumb_image"] =
+            $pageSeo?->breadcrumb_status == 1
+                ? getFile(
+                    $pageSeo->breadcrumb_image_driver,
+                    $pageSeo->breadcrumb_image,
+                )
+                : null;
+
+        $categories = ListingCategory::select(
+            "id",
+            "icon",
+            "mobile_app_image",
+            "image_driver",
+        )
+            ->with(["details:id,listing_category_id,language_id,name"])
+            ->where("status", 1)
+            ->latest()
+            ->get();
+        $categorySingle = DB::table("content_details")
+            ->join("contents", "contents.id", "=", "content_details.content_id")
+            ->where("contents.theme", $selectedTheme)
+            ->where("contents.name", "listing_categories")
+            ->where("contents.type", "single")
+            ->first();
+        return view(
+            template() . "frontend.category.index",
+            compact("pageSeo", "categories", "categorySingle"),
+        );
     }
 
     public function categorySearch(Request $request)
     {
         $character = $request->character;
         if ($character != null) {
-            $data['categories'] = ListingCategory::with('details')->whereHas('details', function ($q) use ($character) {
-                $q->where('name', 'LIKE', $character . '%');
-            })->withCount('get_listings')->where('status', 1)->latest()->get();
+            $data["categories"] = ListingCategory::with("details")
+                ->whereHas("details", function ($q) use ($character) {
+                    $q->where("name", "LIKE", $character . "%");
+                })
+                ->withCount("get_listings")
+                ->where("status", 1)
+                ->latest()
+                ->get();
         } else {
-            $data['categories'] = ListingCategory::with('details')->withCount('get_listings')->where('status', 1)->latest()->get();
+            $data["categories"] = ListingCategory::with("details")
+                ->withCount("get_listings")
+                ->where("status", 1)
+                ->latest()
+                ->get();
         }
-        $count = $data['categories']->count();
-        $view = view($this->theme . 'frontend.category.partials.renderCategory', $data)->render();
-        return response()->json(['data' => $view, 'count' => $count]);
+        $count = $data["categories"]->count();
+        $view = view(
+            $this->theme . "frontend.category.partials.renderCategory",
+            $data,
+        )->render();
+        return response()->json(["data" => $view, "count" => $count]);
     }
-
 }
--- resources/views/themes/light/frontend/category/partials/renderCategory.blade.php
+++ resources/views/themes/light/frontend/category/partials/renderCategory.blade.php
@@ -2,8 +2,8 @@
     <div class="col-lg-3 col-md-4 col-6 ">
         <a href="{{ route('listings',$category->id) }}">
             <div class="category-box d-flex justify-content-start">
-                <div class="icon-box">
-                    <i class="{{ $category->icon }}"></i>
+                <div class="icon-box p-0">
+                    @if(isset($category->mobile_app_image) && $category->mobile_app_image) <img class="avatar avatar-xl avatar-4x3 avatar-centered h-100 mb-2 rounded-circle" src="{{ getFile($category->image_driver, $category->mobile_app_image) }}" alt="{{ optional($category->details)->name }}"/> @else <i class="{{ $category->icon }}"></i> @endif
                 </div>
                 <div>
                     <h5>@lang(optional($category->details)->name)</h5>
