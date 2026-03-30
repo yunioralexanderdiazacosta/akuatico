a40c5da88cb6f50d777e4fe2666db742ae4eac85
Leonardo Gonzalez <gonzalezrujano@gmail.com>
2026-03-30 10:06:47 -0400

    feat

--- resources/views/themes/light/partials/banner.blade.php
+++ resources/views/themes/light/partials/banner.blade.php
@@ -1,4 +1,3 @@
-
 @if(isset($pageSeo) && $pageSeo['breadcrumb_image'])
     <section class="banner-section" style="background-image: url({{ $pageSeo['breadcrumb_image'] }});">
         <div class="overlay">
@@ -6,7 +5,7 @@
                 <div class="row">
                     <div class="col">
                         <div class="header-text text-center">
-                            <h3>@lang($pageSeo['page_title'])</h3>
+                            <!-- <h3>@lang($pageSeo['page_title'])</h3 -->
                         </div>
                     </div>
                 </div>
