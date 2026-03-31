c63fd92c0e623e2023da9b752dc5734dac491c5d
Leonardo Gonzalez <gonzalezrujano@gmail.com>
2026-03-31 17:29:12 -0400

    fix

--- resources/views/themes/light/sections/listing.blade.php
+++ resources/views/themes/light/sections/listing.blade.php
@@ -19,85 +19,88 @@
                 @endif
                 <div class="row g-4">
                     @forelse($listing['popularListings'] as $key => $listing)
-                        @php
-                            $total = $listing->reviews()[0]->total;
-                            $average_review = $listing->reviews()[0]->average;
-			@endphp
-                        <div class="col-lg-3 col-md-6">
-                        <a class="title d-block h-100" href="{{ route('listing.details',$listing->slug) }}">
-                            <div class="listing-box">
-                                <div class="img-box">
-                                    <img class="img-fluid" src="{{ getFile($listing->thumbnail_driver, $listing->thumbnail) }}" alt="image"/>
-                                    <!--button class="save wishList" type="button" id="{{$key}}" data-user="{{ optional($listing->get_user)->id }}"
-                                            data-purchase="{{ $listing->purchase_package_id }}" data-listing="{{ $listing->id }}">
-                                        @if($listing->get_favourite_count > 0)
-                                            <i class="fas fa-heart save{{$key}}"></i>
-                                        @else
-                                            <i class="fal fa-heart save{{$key}}"></i>
-                                        @endif
-                                    </button-->
-                                </div>
-                                <div class="text-box">
-                                    <div class="review">
-                                        @php
-                                            $isCheck = 0;
-                                            $j = 0;
-                                        @endphp
-                                        @if($average_review != intval($average_review))
-                                            @php
-                                                $isCheck = 1;
-                                            @endphp
-                                        @endif
+                            @php
+                                $total = $listing->reviews()[0]->total;
+                                $average_review = $listing->reviews()[0]->average;
+                            @endphp
+                            <div class="col-lg-3 col-md-6">
+                                <a class="title h-100" href="{{ route('listing.details', $listing->slug) }}">
+                                    <div class="listing-box">
+                                        <div class="img-box">
+                                            <img class="img-fluid"
+                                                src="{{ getFile($listing->thumbnail_driver, $listing->thumbnail) }}" alt="image" />
+                                            <!--button class="save wishList" type="button" id="{{$key}}" data-user="{{ optional($listing->get_user)->id }}"
+                                                                    data-purchase="{{ $listing->purchase_package_id }}" data-listing="{{ $listing->id }}">
+                                                                @if($listing->get_favourite_count > 0)
+                                                                    <i class="fas fa-heart save{{$key}}"></i>
+                                                                @else
+                                                                    <i class="fal fa-heart save{{$key}}"></i>
+                                                                @endif
+                                                            </button-->
+                                        </div>
+                                        <div class="text-box">
+                                            <div class="review">
+                                                @php
+                                                    $isCheck = 0;
+                                                    $j = 0;
+                                                @endphp
+                                                @if($average_review != intval($average_review))
+                                                    @php
+                                                        $isCheck = 1;
+                                                    @endphp
+                                                @endif
 
-                                        @for($i = $average_review; $i > $isCheck; $i--)
-                                            <i class="fas fa-star" aria-hidden="true"></i>
-                                            @php
-                                                $j = $j + 1;
-                                            @endphp
-                                        @endfor
+                                                @for($i = $average_review; $i > $isCheck; $i--)
+                                                    <i class="fas fa-star" aria-hidden="true"></i>
+                                                    @php
+                                                        $j = $j + 1;
+                                                    @endphp
+                                                @endfor
 
-                                        @if($average_review != intval($average_review))
-                                            <i class="fas fa-star-half-alt"></i>
-                                            @php
-                                                $j = $j + 1;
-                                            @endphp
-                                        @endif
+                                                @if($average_review != intval($average_review))
+                                                    <i class="fas fa-star-half-alt"></i>
+                                                    @php
+                                                        $j = $j + 1;
+                                                    @endphp
+                                                @endif
 
-                                        @if($average_review == 0 || $average_review != null)
-                                            @for($j; $j < 5; $j++)
-                                                <i class="far fa-star"></i>
-                                            @endfor
-                                        @endif
-                                        <span>(@lang($total.' reviews'))</span>
-                                    </div>
-                                    <p class="title">
-                                        @lang(Str::limit($listing->title, 20))
-                                    </p>
-                                    <p class="mb-2 mt-2">
-                                        <span class="">@lang('Category'): </span> @lang(optional($listing)->getCategoriesName())
-                                    </p>
-                                    <p style="color: var(--primary);">
-                                     @lang(optional($listing->get_user)->firstname) @lang(optional($listing->get_user)->lastname)
-                                    </p>
-                                    <p class="address">
-                                        <i class="fal fa-map-marker-alt"></i>
-                                        @lang($listing->city_id != null ? $listing->get_cities?->getAddress() : $listing->address)
-                                    </p>
-                                </div>
-                        </a>
+                                                @if($average_review == 0 || $average_review != null)
+                                                    @for($j; $j < 5; $j++)
+                                                        <i class="far fa-star"></i>
+                                                    @endfor
+                                                @endif
+                                                <span>(@lang($total . ' reviews'))</span>
+                                            </div>
+                                            <p class="title">
+                                                @lang(Str::limit($listing->title, 20))
+                                            </p>
+                                            <p class="mb-2 mt-2">
+                                                <span class="">@lang('Category'): </span>
+                                                @lang(optional($listing)->getCategoriesName())
+                                            </p>
+                                            <p style="color: var(--primary);">
+                                                @lang(optional($listing->get_user)->firstname)
+                                                @lang(optional($listing->get_user)->lastname)
+                                            </p>
+                                            <p class="address">
+                                                <i class="fal fa-map-marker-alt"></i>
+                                                @lang($listing->city_id != null ? $listing->get_cities?->getAddress() : $listing->address)
+                                            </p>
+                                        </div>
+                                </a>
                             </div>
                         </div>
                     @empty
-                    @endforelse
-                </div>
-                <div class="row text-center mt-5">
-                    <div class="col">
-                        <a href="{{ route('listings') }}" class="btn-custom">
-                            @lang('View More')
-                            <i class="fal fa-angle-double-right"></i></a>
-                    </div>
+                @endforelse
+            </div>
+            <div class="row text-center mt-5">
+                <div class="col">
+                    <a href="{{ route('listings') }}" class="btn-custom">
+                        @lang('View More')
+                        <i class="fal fa-angle-double-right"></i></a>
                 </div>
             </div>
+        </div>
         </div>
     </section>
 @endif
@@ -153,6 +156,3 @@
         });
     }
 </script>
-
-
-
