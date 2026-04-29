f112895c4c7f36c59021b75ad6f280228d2e20c5
Leonardo Gonzalez <gonzalezrujano@gmail.com>
2026-04-29 11:07:21 -0400

    featg

--- resources/views/themes/light/frontend/category/partials/renderCategory.blade.php
+++ resources/views/themes/light/frontend/category/partials/renderCategory.blade.php
@@ -1,9 +1,12 @@
 @foreach ($categories as $category)
     <div class="col-lg-3 col-md-4 col-12">
-        <a href="{{ isset($useProfileRoute) && $useProfileRoute ? route('profiles', ['name' => '', 'category' => [$category->id]]) : route('listings', $category->id) }}">
+        <a href="{{ route('listings') }}?category[]={{ $category->id }}">
             <div class="category-box d-flex justify-content-start">
                 <div class="icon-box p-0">
-                    @if(isset($category->mobile_app_image) && $category->mobile_app_image) <img class="avatar avatar-xl avatar-4x3 avatar-centered h-100 mb-2" src="{{ getFile($category->image_driver, $category->mobile_app_image) }}" alt="{{ optional($category->details)->name }}"/> @else <i class="{{ $category->icon }}"></i> @endif
+                    @if(isset($category->mobile_app_image) && $category->mobile_app_image) <img
+                        class="avatar avatar-xl avatar-4x3 avatar-centered h-100 mb-2"
+                        src="{{ getFile($category->image_driver, $category->mobile_app_image) }}"
+                    alt="{{ optional($category->details)->name }}" /> @else <i class="{{ $category->icon }}"></i> @endif
                 </div>
                 <div>
                     <h5>@lang(optional($category->details)->name)</h5>
--- resources/views/themes/light/frontend/category/specialcategories.blade.php
+++ resources/views/themes/light/frontend/category/specialcategories.blade.php
@@ -1,4 +1,4 @@
-@extends(template().'layouts.app')
+@extends(template() . 'layouts.app')
 @section('title', 'directorio')
 
 @section('content')
@@ -6,16 +6,15 @@
       <section class="category-filter-section">
          <div class="container">
             <div class="row g-3 mt-5" id="renderCategory">
-                @include(template().'frontend.category.partials.renderCategory', ['useProfileRoute' => true])
+               @include(template() . 'frontend.category.partials.renderCategory')
             </div>
          </div>
       </section>
-      @else
-       <div class="custom-not-found2">
-           <img src="{{ asset(template(true).'img/no_data_found.png') }}" alt="{{ basicControl()->site_title }}"
-                class="img-fluid">
-       </div>
+   @else
+      <div class="custom-not-found2">
+         <img src="{{ asset(template(true) . 'img/no_data_found.png') }}" alt="{{ basicControl()->site_title }}"
+            class="img-fluid">
+      </div>
    @endif
 
 @endsection
-
